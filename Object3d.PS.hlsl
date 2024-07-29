fdf674f806f4d029adffaa5808d870026b178f5c
tojoyoshiki <146914549+tojoyoshiki@users.noreply.github.com>
2024-07-29 14:01:14 +0900

    06-03完了

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -35,10 +35,7 @@ PixelShaderOutput main(VertexShaderOutput input)
 
     // テクスチャサンプルを行う
     float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
-
-    // ピクセルの色を計算する
-    //output.color = gMaterial.color * textureColor;
-
+    
     if (gMaterial.enableLighting != 0)
     {
         float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
