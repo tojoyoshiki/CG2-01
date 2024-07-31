c4c2d448106a715c3e65f3de67496d525a535e34
tojoyoshiki <146914549+tojoyoshiki@users.noreply.github.com>
2024-07-31 17:59:24 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1254,6 +1254,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	//Textureを読んで転送する
 	DirectX::ScratchImage mipImages = LoadTexture("resources/uvChecker.png");
+	//DirectX::ScratchImage mipImages = LoadTexture("uvChecker.png");
 	const DirectX::TexMetadata& metadata = mipImages.GetMetadata();
 	ID3D12Resource* textureResource = CreateTextureResource(device, metadata);
 	UploadTextureData(textureResource, mipImages);
