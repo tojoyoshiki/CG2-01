0f21b6be888af18b785b4f597f9811d9df78c914
tojoyoshiki <k023g0101@g.neec.ac.jp>
2024-10-07 16:02:45 +0900

    debug、release両方動く

---
+++ Input.cpp
@@ -0,0 +1,18 @@
+//#include "Input.h"
+//
+//void Input::Initialize()
+//{
+//	IDirectInput8* directInput = nullptr;
+//	result = DirectInput8Create(
+//		w.hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8,
+//		(void**)&directInput, nullptr);
+//	assert(SUCCEEDED(result));
+//}
+//
+//void Input::Update()
+//{
+//}
+//
+//void Input::Draw()
+//{
+//}
---
+++ Input.h
@@ -0,0 +1,16 @@
+//#pragma once
+//#define DIRECTINPUT_VERSION 0x0800
+//#include <dinput.h>
+//#include <cassert>
+// 
+//#pragma comment(lib,"dinput8.lib")
+//#pragma comment(lib,"dxguid.lib")
+//
+//class Input
+//{
+//	void Initialize();
+//	void Update();
+//	void Draw();
+//
+//};
+//
